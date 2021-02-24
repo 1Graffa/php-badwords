c6dff20abd2a50ef53b7497ebfd25d47d1d9609f
Giuseppe <giusepperaffa1988@gmail.com>
2021-02-24 18:23:17 +0100

    string replace

--- script.php
+++ script.php
@@ -1,5 +1,6 @@
 <?php
-$nome = "PHP";
+$testo = "questo è il mio testo";
+//$badword = $_GET[word];
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,7 +11,8 @@ $nome = "PHP";
     <title>Badwords</title>
 </head>
 <body>
-    <h1><?php echo $nome?></h1>
-    
+    <h1><?php echo $testo?></h1>
+    <h2>Lunghezza : <?php echo strlen($testo)?></h2>
+    <h3><em>Risultato : </em>"<?php echo ucfirst(str_replace('mio' , '***', $testo))?>" dopo str_replace</h3>
 </body>
 </html>
